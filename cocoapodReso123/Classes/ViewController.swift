b33d056b7e03eb76814548ff0b4cc9585a8534ed
vishnuprasannan333 <vishnups333@gmail.com>
2017-07-04 15:32:20 +0530

    Update ViewController.swift

--- cocoapodReso123/Classes/ViewController.swift
+++ cocoapodReso123/Classes/ViewController.swift
@@ -1,24 +1,5 @@
-//
-//  ViewController.swift
-//  fortesting
-//
-//  Created by HANIF on 7/4/17.
-//  Copyright © 2017 bmuse. All rights reserved.
-//
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class ViewController {
 
 
 }
